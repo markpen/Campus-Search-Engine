621c942e1357efb1f1f155a96585b2183e3e975b
markpen <exiletheworld@gmail.com>
2018-01-13 21:00:01 -0800

    word frequencies

---
+++ Campus Search Engine/src/edu/uci/cs/searchengine/wordfrequencies/WordSet.java
@@ -0,0 +1,56 @@
+package edu.uci.cs.searchengine.wordfrequencies;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class WordSet {
+
+	// get a intersection of two files
+	public HashSet<String> intersectionWords(String file1, String file2) {
+		HashSet<String> intersections = new HashSet<String>();
+		WordFrequency wf1 = new WordFrequency();
+		WordFrequency wf2 = new WordFrequency();
+		HashMap<String, Integer> file1Words = wf1.computerWordFrequencies(wf1.tokenize(file1));
+		HashMap<String, Integer> file2Words = wf2.computerWordFrequencies(wf2.tokenize(file2));
+			
+		for(String key: file1Words.keySet()) {
+			if(file2Words.containsKey(key)) intersections.add(key);
+		}
+			
+		return intersections;
+	}
+		
+	// get a reduce of two files
+	public HashMap<String, Integer> reduceWords(String file1, String file2) {
+		WordFrequency wf1 = new WordFrequency();
+		WordFrequency wf2 = new WordFrequency();
+		HashMap<String, Integer> file1Words = wf1.computerWordFrequencies(wf1.tokenize(file1));
+		HashMap<String, Integer> file2Words = wf2.computerWordFrequencies(wf2.tokenize(file2));
+		
+		for(String word : file1Words.keySet()) {
+			if(file2Words.containsKey(word)) {
+				int freq = file2Words.get(word);
+				file2Words.replace(word, freq + file1Words.get(word));
+			} else {
+				file2Words.put(word, file1Words.get(word));
+			}
+		}
+			
+		return file2Words;
+	}	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WordFrequency wf = new WordFrequency();
+		WordSet ws = new WordSet();
+		String fileName = "/home/markpen/Desktop/CompSci 221/Project 1/Word Frequency/src/text.txt";
+		String fileName2 = "/home/markpen/Desktop/CompSci 221/Project 1/Word Frequency/src/text2.txt";
+		
+		List<String> words = wf.tokenize(fileName);
+		HashMap<String, Integer> frequencies = ws.reduceWords(fileName, fileName2);
+		//HashMap<String, Integer> frequencies = wf.computerWordFrequencies(words);
+		wf.print(frequencies);
+		
+	}
+}
